3e0b95700666716f2b936070442cd6e5aec138b9
Vincent-ka <vincent.ka2893@gmail.com>
2021-01-10 19:36:47 +0100

    modification du controller dj

--- src/controllers/djs_controller.js
+++ src/controllers/djs_controller.js
@@ -1,33 +1,71 @@
-const { pick } = require("lodash");
+const {
+  pick
+} = require("lodash");
 
-const { Djs, Musicalgenres, DjMusicalgenres } = require("../../models");
-const { NotFoundError } = require("../helpers/errors");
+const {
+  Djs,
+  Musicalgenres,
+  DjMusicalgenres,
+  Clubs
+} = require("../../models");
+const {
+  NotFoundError
+} = require("../helpers/errors");
+const musicalgenres = require("../../models/musicalgenres");
 
 const djsController = {
   getAllDjs: async () => {
-    const djsFound = await Djs.findAll();
-    console.log(djsFound)
-    return djsFound;
+    const allDjs = await Djs.findAll({
+      include: [Clubs, Musicalgenres]
+    })
+    return {
+      allDjs
+    };
   },
 
   getDj: async (name) => {
-    const djFound = await Djs.findOne(name);
-    return {djFound};
+    const oneDj = await Djs.findOne({
+      where: {
+        name: name
+      }
+    })
+    return {
+      oneDj
+    };
   },
 
   addDj: async (data) => {
-    const djCreated = await Djs.create(data);
-    return djCreated
+
+    const newDj = await Djs.create(data, {
+      through: data.musical_genres
+    })
+    console.log("------", data.musical_genres);
+    return {
+      newDj
+    };
   },
 
+
   updateDj: async (name, data) => {
-    const djUpdated = await Djs.save(name);
-    return {djUpdated}
+    const updateDj = await Djs.update(data, {
+      where: {
+        name: name
+      }
+    })
+    return {
+      updateDj
+    };
   },
 
   deleteDj: async (name) => {
-    const djDeleted = await Djs.destroy(name);
-    return {djDeleted}
+    const deleteDj = await Djs.destroy({
+      where: {
+        name: name
+      }
+    })
+    return {
+      deleteDj
+    };
   },
 };
 
